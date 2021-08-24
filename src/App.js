6cdd41272653b68784d4e56fa96f13908ef0c143
Matias Chappet <matiaschappet@gmail.com>
2021-08-24 19:02:34 -0300

    init reducer home y nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Route } from 'react-router-dom'
 import React from 'react';
+import Nav from './components/Nav/Nav';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <React.Fragment>
-      
+      <Route path='/' component={Nav} />
+      <Route path='/home' component={Home} />
     </React.Fragment>
   );
 }
---
+++ src/reducer/index.js
@@ -0,0 +1,12 @@
+
+
+const initialState = {
+    cities: []
+}
+
+export default function reducer(state = initialState, action){
+    switch(action.type){
+        default:
+            return state
+    }
+}
