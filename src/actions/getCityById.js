23d471a9ef2fdd438459047168db2e53dcf196f0
Matías Chappet <83678198+Chappetm@users.noreply.github.com>
2021-08-29 00:54:55 -0300

    Update getCityById.js

--- src/actions/getCityById.js
+++ src/actions/getCityById.js
@@ -3,7 +3,7 @@ import { GET_CITY_BY_ID} from "./const"
 
 export default function getCityById(id){
     return async function(dispatch){
-        const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?id=${id}&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
         dispatch({
             type: GET_CITY_BY_ID,
             payload: response.data
