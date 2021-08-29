8666b6c55b295f01067d4435fd45da0557c4ea5e
Matías Chappet <83678198+Chappetm@users.noreply.github.com>
2021-08-29 00:54:11 -0300

    Update getCityByCoords.js

--- src/actions/getCityByCoords.js
+++ src/actions/getCityByCoords.js
@@ -4,7 +4,7 @@ import {GET_BY_COORD} from './const'
 
 export default function getCityByCoords(lon, lat){
     return async function(dispatch){
-        const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
         console.log('action')
         dispatch({
             type: GET_BY_COORD,
